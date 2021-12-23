a23f2f20b08eacd19f64d896215891a848397bb1
32248480 <92283556+32248480@users.noreply.github.com>
2021-12-23 17:37:51 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void main(String args[]){
+String str;
+ str=Float.toString(123.45f);
+ System.out.println(str);
+ System.out.println("length="+str.length());
+}
+}
+
+//123.45
+//length=6
